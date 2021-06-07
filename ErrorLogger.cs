5110669e1bf035128c614936a21e5cd1984fe17b
Matthew Cullen <m23.cullen@gmail.com>
2021-06-07 16:36:48 -0700

    Update ErrorLogger.cs

--- ErrorLogger.cs
+++ ErrorLogger.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+//// EXAMPLE OF USE
+// string filePath = System.IO.Path.GetFullPath(@"..\logs.txt");
+// ErrorLogger eLogger = new ErrorLogger(filePath);
+
+// string errorMsg = "new error";
+// DateTime date = DateTime.UtcNow;
+
+// eLogger.LogNewError(errorMsg, date);
+// eLogger.DisplayErrorsToConsole();
+
 // HOW TO ADD/REMOVE PARAMS:
 // 1. UPDATE ERROR CLASS
 // 2. ADD NEW PARAMS TO THESE METHODS `LOGNEWERRORS, CREATEERROROBJECTS, DISPLAYERRORS TO CONSOLE`
